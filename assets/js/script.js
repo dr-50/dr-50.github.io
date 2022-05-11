1949a0b408bd2d9af284306ed36ef3bd070b73c4
Devon <devonross50@yahoo.com>
2022-05-11 10:12:05 -0500

    updated with new work role

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,24 @@ function openNav() {
   } 
   })
   // End NavBar
+
+  //display color change
+  var timelineItem = document.getElementsByClassName("timeline-item");
+  var resumeDownload = document.getElementsByClassName("resume-download");
+ 
+  function darkDisplay(){
+      
+      document.body.classList.remove("colorful-display-color");
+      document.body.classList.add("dark-display-color");
+      $(timelineItem).removeClass("timeline-colorful-display");
+      $(timelineItem).addClass("timeline-dark-display");
+      $(resumeDownload).addClass("dark-display-color");
+  }
+
+  function colorfulDisplay(){
+      document.body.classList.remove("dark-display-color");
+      document.body.classList.add("colorful-display-color");
+      $(timelineItem).removeClass("timeline-dark-display");
+      $(timelineItem).addClass("timeline-colorful-display");
+      $(resumeDownload).addClass("dark-display-color");
+  }
