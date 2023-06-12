0428d43b50d536a993eb4b762226720a92679958
Devon <devonross50@yahoo.com>
2023-06-12 14:58:35 -0500

    updated background width to fit phones

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,31 @@
 
-// NavBar
-function openNav() {
-    document.getElementById("mySidepanel").style.width = "375px";
-  }
+// About Me (biography/bio) activate/inactivate
+function bioActivate(event) {
+  var elems = document.querySelector('.bio-nav-cards').querySelectorAll(".active");
+  [].forEach.call(elems, function(el) {
+    el.classList.remove("active");
+  });
+  //add active class
+  console.log(event.target.className)
+  event.target.className += ' active';
   
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  }
-  
-  window.addEventListener('click', function(e){
-    if (!document.getElementById('mySidepanel').contains(e.target) && (!document.getElementById('openbtn').contains(e.target))){
-    document.getElementById("mySidepanel").style.width = "0";
-  } 
-  })
-  // End NavBar
 
-  
+  //remove active class from paragraphs
+  var elements = document.querySelector('.details-card').querySelectorAll('.active');
+  [].forEach.call(elements, function(els){
+    els.classList.remove('active')
+  });
+  //add active class to selected
+  selectedValue = event.target.innerHTML
+  activeParagraph = document.getElementsByClassName(selectedValue);
+  activeParagraph[0].className += ' active'
+
+}
+
+// function menuActivate(event){
+//   var elms = document.querySelector('.details-card').querySelectorAll('.active');
+//   [].forEach.call(elms, function(el) {
+//     el.classList.remove('active');
+//   });
+//   event.target.class += active
+// }
