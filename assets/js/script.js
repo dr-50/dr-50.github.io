555211bc90dc48916aa60b12641ae0396417a24d
Devon <devonross50@yahoo.com>
2023-06-19 06:44:53 -0500

    commit for checkout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,7 @@
 
-// About Me (biography/bio) activate/inactivate
+// About Me (biography/bio) activate/inactivate past/current/future buttons
 function bioActivate(event) {
-  var elems = document.querySelector('.bio-nav-cards').querySelectorAll(".active");
-  [].forEach.call(elems, function(el) {
-    el.classList.remove("active");
-  });
-  //add active class
-  console.log(event.target.className)
-  event.target.className += ' active';
   
-
   //remove active class from paragraphs
   var elements = document.querySelector('.details-card').querySelectorAll('.active');
   [].forEach.call(elements, function(els){
@@ -20,12 +12,62 @@ function bioActivate(event) {
   activeParagraph = document.getElementsByClassName(selectedValue);
   activeParagraph[0].className += ' active'
 
+  //remove active class from buttons
+  var elems = document.querySelector('.bio-nav-cards').querySelectorAll(".active");
+  [].forEach.call(elems, function(el) {
+    el.classList.remove("active");
+  });
+  //add active class to selected button
+  event.target.className += ' active';
+  
+
 }
 
-// function menuActivate(event){
-//   var elms = document.querySelector('.details-card').querySelectorAll('.active');
-//   [].forEach.call(elms, function(el) {
-//     el.classList.remove('active');
-//   });
-//   event.target.class += active
-// }
+
+//page navigation activate/inactive aboutme/portfoio/contactme/resume
+function navActivate(element){
+  //remove active class from detail-card body(switch view from nav)
+  var elems = document.querySelectorAll('.details-card');
+  [].forEach.call(elems, function(el){
+    el.classList.remove('active')
+  })
+  //add active class to selected nav (switch view from nav)
+  //used a switch to identify button clicked id and align with order of expected results. Enhancements needed to increase logic ere
+  var el = element.id
+  console.log(el)
+  switch(el){
+    case "1":
+      //since getElementsByClassName returns a collection of elements must included [0]
+      document.getElementsByClassName("details-card bio")[0].className += ' active';
+      break;
+    case "2":
+
+      document.getElementsByClassName("details-card portfolio")[0].classList += ' active';
+      break;
+    case "3":
+      document.getElementsByClassName("details-card contact")[0].className += ' active';
+
+      break;
+    case "4":
+      document.getElementsByClassName("details-card resume")[0].className += ' active';
+  }
+
+
+
+  //remove active class from button
+  var elements = document.querySelector('.nav-cards').querySelectorAll('.active');
+  [].forEach.call(elements, function(els){
+    els.classList.remove('active')
+  })
+  
+  //add active class to button
+  event.target.className += ' active'
+
+}
+
+
+function copyContact(element){
+  var copyText = element.previousElementSibling.innerHTML
+  var copyTextLen = copyText.length
+  navigator.clipboard.writeText(copyText.substring(2,copyTextLen))
+}
